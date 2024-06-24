eb1d9535553bb84978cdc25194fe6572ff91b84d
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-06-24 12:24:51 -0700

    post data added

---
+++ index.js
@@ -0,0 +1,29 @@
+const posts = [
+    {
+        name: "vincent van Gogh",
+        username: "vincy1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangough.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4
+    },
+    {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        locations: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152
+    }
+]
