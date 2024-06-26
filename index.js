c38c0329b9e9af2d634357fd59662c02a9af46db
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-06-25 18:29:27 -0700

    implemented for loop to render post data

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const posts = [
     {
-        name: "vincent van Gogh",
+        name: "Vincent van Gogh",
         username: "vincy1853",
         location: "Zundert, Netherlands",
         avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangough.jpg",
+        post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21
     },
@@ -20,10 +20,51 @@ const posts = [
     {
         name: "Joseph Ducreux",
         username: "jd1735",
-        locations: "Paris, France",
+        location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
 ]
+
+const postsArea = document.getElementById("user-posts-area")
+
+window.onload = function() {
+    postsArea.innerHTML = ""
+
+    for (let i = 0; i < posts.length; i++) {
+        let nameEl = Object.values(posts)[i].name
+        let usernameEl = Object.values(posts)[i].username
+       let locationEl = Object.values(posts)[i].location
+       let avatarImg = Object.values(posts)[i].avatar
+       let postImg = Object.values(posts)[i].post
+       let commentEl = Object.values(posts)[i].comment
+       let likesEl = Object.values(posts)[i].likes
+
+        postLayout(nameEl, usernameEl, locationEl, avatarImg, postImg, commentEl, likesEl)
+        
+    }
+}
+
+function postLayout(name, username, location, avatar, post, comment, likes) {
+        postsArea.innerHTML +=  `
+         <article class="post">
+            <div class="user-info">
+                <img class="user-avatar" src=${avatar}>
+                <div class="name-location">
+                    <h2>${name}</h2>
+                    <p>${location}</p>
+                </div>
+            </div>
+            <img class="post-image" src=${post}>
+            <div class="social-btns">
+                <button><img src="/images/icon-heart.png" class="social-icons heart"></button>
+                <button><img src="/images/icon-comment.png" class="social-icons comment"></button>
+                <button><img src="/images/icon-dm.png" class="social-icons dm"></button>
+            </div>
+            <p class="like-element">${likes} likes</p>
+            <p class="post-text"><span class="username">${username}</span> ${comment}</p>
+        </article>
+        `
+}
